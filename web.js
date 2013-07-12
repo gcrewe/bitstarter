b4172cd49c79543c9bfc628833990914b17f08c7
gcrewe <george.crewe@gmail.com>
2013-07-12 20:52:44 +0000

    Added to read phrase from file.

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+
+var fs = require("fs");
+ 
+var greeting = fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString();
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(greeting);
 });
 
 var port = process.env.PORT || 5000;
